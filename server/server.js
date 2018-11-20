e6aa4fecf7e137fbf9c1a4f11b7897017e5b1175
Lovepreet Singh <lovep1730@gmail.com>
2018-11-20 21:08:59 +0530

    Added POST  /todos and refactored the code.

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+})
