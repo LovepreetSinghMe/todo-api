bd97328e0b48dc22d4b0cc1042331829f2e6641a
Lovepreet Singh <lovep1730@gmail.com>
2018-11-21 18:38:31 +0530

    Add tests GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second todo'
 }];
 
@@ -71,5 +74,36 @@ describe('GET /todos', () => {
         })
         .end(done);
 
+    });
+
+});
+
+describe('GET /todos:id', () => {
+
+    it('should get todo with the specified valid id', (done) => {
+
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should respond with 404 when no record found', (done) => {
+
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should respond with 404 when id is invalid', (done) => {
+
+        request(app)
+            .get('/todos/123abc')
+            .expect(404)
+            .end(done);
     });
 });
