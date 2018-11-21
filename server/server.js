403cad089c41fbb9a54c2ec820173477606dfe81
Lovepreet Singh <lovep1730@gmail.com>
2018-11-21 10:11:39 +0530

    Added tests for GET /todos. FIxed response in the case of error.

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
-        res.send(400).send(e);
+        res.status(400).send(e);
     });
 });
 
