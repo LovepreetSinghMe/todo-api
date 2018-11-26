288490205bb9893a662797bd624557af1233e2ef
Lovepreet Singh <lovep1730@gmail.com>
2018-11-26 12:28:51 +0530

    Added POST /users/login.

--- server/models/user.js
+++ server/models/user.js
@@ -66,6 +66,24 @@ userSchema.statics.findByToken = function(token) {
     });
 };
 
+userSchema.statics.findByCredentials = function(email, password) {
+    return this.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 userSchema.pre('save', function(next) {
     if(this.isModified('password')) {
         bcrypt.genSalt(10, (err, salt) => {
